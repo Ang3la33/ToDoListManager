58f1510151f6d91692961b1e98b1976a64a2aabd
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-02-25 00:41:14 -0330

    Added notes to ToDoListCLI for what needs to be completed

--- src/ToDoListCLI.java
+++ src/ToDoListCLI.java
@@ -1,2 +1,18 @@
+
+// To Do List Manager Command Line Interface
+// Will execute the Linked List (Task List)
+// Will use a User ArrayList to store users
+
+// Create a User Menu where a user can
+// Add a user
+// Add tasks to a user
+// Mark tasks as complete
+// View all tasks for user
+
+import models.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ToDoListCLI {
 }
