c11f2ac63a304a6e3e510219213c653c867f9607
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-02-24 23:49:45 -0330

    Created a user class with functions implement from TaskList class

--- src/User.java
+++ src/User.java
@@ -1,2 +1,34 @@
 public class User {
+
+    private int userID;
+    private int IDcounter = 1;
+    private String userName;
+    private TaskList taskList;
+
+    public User(String userName) {
+        this.userName = userName;
+        this.userID = IDcounter++;
+        taskList = new TaskList();
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void addTask(String task) {
+        taskList.addTask(new Task(task));
+    }
+
+    public void markTaskAsComplete(int taskID) {
+        taskList.markTaskAsComplete(taskID);
+    }
+
+    public void printTaskList() {
+        System.out.println("Task List for " + userName + ":");
+        taskList.displayTasks();
+    }
 }
