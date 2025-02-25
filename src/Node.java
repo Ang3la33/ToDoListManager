cc4503172000b26a0add961541e282a2c48ccd2d
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-02-24 23:55:27 -0330

    Created Node class for linked list - handles the linking structure of the Task List

---
+++ src/Node.java
@@ -0,0 +1,10 @@
+// Node class to handle the linking structure of the Task List
+public class Node {
+    Task task;
+    Node next;
+
+    public Node(Task task) {
+        this.task = task;
+        this.next = null;
+    }
+}
